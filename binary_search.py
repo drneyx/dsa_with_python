63deeb9694b40ad407c7f29307e72eac2a277c53
Nathani Luving <cosmasnathan9@gmail.com>
2022-11-17 23:34:05 +0300

    implement binary search

---
+++ binary_search.py
@@ -0,0 +1,30 @@
+def binary_search(list, target):
+
+    first = 0
+    last = len(list) - 1
+
+    while first <= last:
+
+        midpoint = (first + first) // 2
+
+        if list[midpoint] == target:
+            return midpoint
+        elif list[midpoint] < target:
+            first = midpoint + 1 
+        else:
+            last = midpoint - 1
+    return None
+
+
+def verify(index):
+    if index is not None:
+        print("Item found at ", index)
+    else:
+        print("Item not found")
+
+result = binary_search(numbers, 12)
+ver = verify(result)
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+result = binary_search(numbers, 7)
+ver = verify(result)
